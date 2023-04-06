4d95a54188af792ece38360c79708a9682ca8036
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-04-07 03:11:25 +0800

    form not responsive

--- src/components/SubjectAssignPage/SubjectAssignPage.jsx
+++ src/components/SubjectAssignPage/SubjectAssignPage.jsx
@@ -299,12 +299,14 @@ const SubjectAssignPage = () => {
                     <OutlinedInput />
                   )
                 }
+                
               >
                 <MenuItem value={10}>Ten</MenuItem>
                 <MenuItem value={20}>Twenty</MenuItem>
                 <MenuItem value={30}>Thirty</MenuItem>
               </Select>
             </FormControl>
+
 
             {/* COURSE TITLE AND CODE FIELD */}
           <div className={SubjectAssignCSS["course-title-field"]}>
