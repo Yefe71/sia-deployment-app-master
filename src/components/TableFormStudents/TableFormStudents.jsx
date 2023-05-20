33f9dbeec3526c9cc3f35c85fc31695a9d4f1b5c
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-05-21 02:24:14 +0800

    Row disabled when plotted

--- src/components/TableFormStudents/TableFormStudents.jsx
+++ src/components/TableFormStudents/TableFormStudents.jsx
@@ -3,7 +3,7 @@ import React, {
   useEffect,
   useState,
   useImperativeHandle,
-  useRef
+  useRef,
 } from "react";
 import {
   Table,
@@ -12,13 +12,13 @@ import {
   TableCell,
   TableBody,
   TablePagination,
-  ButtonBase
+  ButtonBase,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
 import * as XLSX from "xlsx";
-
+import dayjs from "dayjs";
 
 const dataRowSX = {
   display: "table-row",
@@ -26,23 +26,27 @@ const dataRowSX = {
     backgroundColor: "#f5f5f5",
     cursor: "pointer",
   },
-  '&.clicked': {
-    animation: '$blink 0.5s ease-in-out',
+  "&.disabled": {
+    backgroundColor: "#f5f5f5",
+    cursor: "not-allowed",
   },
-  '@keyframes blink': {
-    '0%': { backgroundColor: 'transparent' },
-    '50%': { backgroundColor: '#f5f5f5' },
-    '100%': { backgroundColor: 'transparent' },
+  "&.clicked": {
+    animation: "$blink 0.5s ease-in-out",
+  },
+  "@keyframes blink": {
+    "0%": { backgroundColor: "transparent" },
+    "50%": { backgroundColor: "#f5f5f5" },
+    "100%": { backgroundColor: "transparent" },
   },
 };
+
+
 const StyledTableCellID = styled(TableCell)({
   fontWeight: "bold",
   backgroundColor: "#f7f4f4",
 });
 
-const StyleTable = styled(Table)({
-
-});
+const StyleTable = styled(Table)({});
 
 const StyledTableCellLeft = styled(TableCell)({
   fontWeight: "bold",
@@ -71,28 +75,75 @@ const StyledTableHead = styled(TableHead)({
   zIndex: 1,
 });
 
-const StickyPagination = styled('div')({
-  position: 'sticky',
+const StickyPagination = styled("div")({
+  position: "sticky",
   bottom: 0,
-  width: '100%',  // make sure it stretches to full width
-  backgroundColor: '#f7f4f4',
+  width: "100%", // make sure it stretches to full width
+  backgroundColor: "#f7f4f4",
   zIndex: 1,
-  padding: "1px 0px"
+  padding: "1px 0px",
 });
 
 const TableFormStudents = forwardRef(
-  (
-    {
-      yearTableProp,
-      handleRowClick
-    },
-    ref
-  ) => {
+  ({ yearTableProp, handleRowClick }, ref) => {
     const [data, setData] = useState([]);
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(7);
 
     const [clickedRow, setClickedRow] = useState(null);
+    const [schedules, setSchedules] = useState([]);
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`http://localhost:3000/grabSchedules`);
+          const data = await response.json();
+          const rows = data.map((item) => ({
+            id: item.id,
+            color: item.color,
+            startDate: item.start_date,
+            endDate: item.end_date,
+            professorName: item.professor_name,
+            year: item.year,
+            block: item.block,
+            courseName: item.course_name,
+            courseCode: item.course_code,
+            actualUnits: item.actual_units,
+            units: item.units,
+            classType: item.class_type,
+            room: item.room,
+            day: dayjs(item.day).format("YYYY-MM-DD"),
+          }));
+
+          setSchedules(rows);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      fetchData();
+    }, []);
+
+    useEffect(() => {
+      console.log(data, "schedules!");
+    }, [data]);
+
+    const isScheduleConflict = (row) => {
+      return schedules.some(
+        (schedule) =>
+          schedule.year === row.year &&
+          schedule.block === row.block &&
+          schedule.courseCode === row.course_code &&
+          schedule.professorName === row.professor_name &&
+          schedule.room === row.room &&
+          schedule.day === dayjs(row.day).format("YYYY-MM-DD") &&
+          dayjs(schedule.startDate, "h:mm A").isSame(dayjs(row.start_date, "h:mm A")) &&
+          dayjs(schedule.endDate, "h:mm A").isSame(dayjs(row.end_date, "h:mm A"))
+      );
+    };
+
+
+
+
 
     const handleClick = (id) => {
       setClickedRow(id);
@@ -100,17 +151,14 @@ const TableFormStudents = forwardRef(
     };
 
     const fetchDataButtonsYear = () => {
-      fetch(
-        `http://localhost:3000/grabMinorsData?yearButton=${yearTableProp}`
-      )
+      fetch(`http://localhost:3000/grabMinorsData?yearButton=${yearTableProp}`)
         .then((response) => response.json())
         .then((data) => {
           setData(data);
-          console.log(data)
+          console.log(data);
         })
         .catch((error) => console.log(error));
     };
-
 
     const handleChangePage = (event, newPage) => {
       setPage(newPage);
@@ -121,7 +169,6 @@ const TableFormStudents = forwardRef(
       setPage(0);
     };
 
-
     useEffect(() => {
       fetchDataButtonsYear();
       setPage(0);
@@ -131,7 +178,7 @@ const TableFormStudents = forwardRef(
     const getDayLabel = (dateString) => {
       const date = new Date(dateString);
       date.setUTCDate(date.getUTCDate() + 1);
-      const datePart = date.toISOString().split('T')[0];
+      const datePart = date.toISOString().split("T")[0];
 
       const dayLabels = [
         { value: "2023-01-02", label: "Mon" },
@@ -140,17 +187,13 @@ const TableFormStudents = forwardRef(
         { value: "2023-01-05", label: "Thu" },
         { value: "2023-01-06", label: "Fri" },
         { value: "2023-01-07", label: "Sat" },
-        { value: "2023-01-08", label: "Sun" }
+        { value: "2023-01-08", label: "Sun" },
       ];
 
-      const dayLabel = dayLabels.find(day => day.value === datePart);
+      const dayLabel = dayLabels.find((day) => day.value === datePart);
 
-      return dayLabel ? dayLabel.label : 'Date not found';
+      return dayLabel ? dayLabel.label : "Date not found";
     };
-
-
-
-
 
     function formatTime(dateString) {
       const date = new Date(dateString);
@@ -158,20 +201,19 @@ const TableFormStudents = forwardRef(
       const minutes = date.getMinutes();
 
       // Determine if it's AM or PM
-      const ampm = hours >= 12 ? 'PM' : 'AM';
+      const ampm = hours >= 12 ? "PM" : "AM";
 
       // Convert hours to 12-hour format
       const formattedHours = hours % 12 || 12;
 
       // Add leading zeros to minutes if needed
-      const formattedMinutes = minutes.toString().padStart(2, '0');
+      const formattedMinutes = minutes.toString().padStart(2, "0");
 
       // Concatenate the formatted time string
       const formattedTime = `${formattedHours}:${formattedMinutes} ${ampm}`;
 
       return formattedTime;
     }
-
 
     return (
       <>
@@ -186,51 +228,76 @@ const TableFormStudents = forwardRef(
                 <StyledTableCell>Course Code</StyledTableCell>
 
                 <StyledTableCell>Room</StyledTableCell>
+                <StyledTableCell>Class Type</StyledTableCell>
                 <StyledTableCell>Day</StyledTableCell>
                 <StyledTableCell>Start Time</StyledTableCell>
                 <StyledTableCellRight>End Time</StyledTableCellRight>
-       
               </StyledTableRow>
             </StyledTableHead>
             <TableBody>
-              {data
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => (
-                   <ButtonBase
+            {data
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index) => {
+                const matchingSchedule = schedules.find(schedule => 
+                  schedule.year === row.year &&
+                  // schedule.block === row.block &&
+                  schedule.courseCode === row.course_code &&
+                  schedule.professorName === row.professor_name &&
+                  schedule.room === row.room &&
+                  schedule.classType === row.class_type  &&
+                  dayjs(schedule.day).format("YYYY-MM-DD") === dayjs(row.day).format("YYYY-MM-DD")  &&
+                  schedule.startDate === row.start_date &&
+                  schedule.endDate === row.end_date
+                );
+
+                const isDisabled = !!matchingSchedule;  // If matchingSchedule exists, isDisabled will be true.
+
+                const dataRowSXDisabled = {
+                  ...dataRowSX,
+                  pointerEvents: isDisabled ? 'none' : 'auto',
+                  cursor: isDisabled ? 'not-allowed' : 'pointer',
+                  opacity: isDisabled ? 0.5 : 1,
+                };
+
+                return (
+                  <ButtonBase
                     key={row.id}
                     component={TableRow}
-                    sx={dataRowSX}
+                    sx={isDisabled ? dataRowSXDisabled : dataRowSX}
                     onClick={() => {
-                      handleRowClick(row)
-                      handleClick(row.id)
+                      if (!isDisabled) {
+                        handleRowClick(row);
+                        handleClick(row.id);
+                      }
                     }}
                     className={row.id === clickedRow ? 'clicked' : ''}
                   >
-            
                     <TableCell>{page * rowsPerPage + index + 1}</TableCell>
                     <TableCell>{row.professor_name}</TableCell>
                     <TableCell>{row.year}</TableCell>
                     <TableCell>{row.course_name}</TableCell>
                     <TableCell>{row.course_code}</TableCell>
                     <TableCell>{row.room}</TableCell>
+                    <TableCell>{row.class_type}</TableCell>
                     <TableCell>{getDayLabel(row.day)}</TableCell>
                     <TableCell>{formatTime(row.start_date)}</TableCell>
                     <TableCell>{formatTime(row.end_date)}</TableCell>
-                    </ButtonBase>
-                ))}
+                  </ButtonBase>
+                );
+              })}
             </TableBody>
           </Table>
         </StyleTable>
         <StickyPagination>
-        <TablePagination
-          component="div"
-          count={data.length}
-          page={page}
-          onPageChange={handleChangePage}
-          rowsPerPage={rowsPerPage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-          rowsPerPageOptions={[7, 14, 21, 28, 35]}
-        />
+          <TablePagination
+            component="div"
+            count={data.length}
+            page={page}
+            onPageChange={handleChangePage}
+            rowsPerPage={rowsPerPage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            rowsPerPageOptions={[7, 14, 21, 28, 35]}
+          />
         </StickyPagination>
       </>
     );
