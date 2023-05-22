9847e99417a34fb3ca3e3eee0e2743bc419c64df
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-05-22 17:49:19 +0800

    ui done responsive study plan

--- src/components/StudyPlan/StudyPlan.jsx
+++ src/components/StudyPlan/StudyPlan.jsx
@@ -3,12 +3,13 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import StudyPlanCSS from "./StudyPlan.module.css";
-import { useMediaQuery } from "@mui/material";
+import { Button, Stack, useMediaQuery } from "@mui/material";
 import ProfessorTable from "../ProfessorTable/ProfessorTable";
 import StudyPlanForm from "../StudyPlanForm/StudyPlanForm";
 import TableFormStudents from "../TableFormStudents/TableFormStudents";
 import TableManageBlock from "../TableManageBlock/TableManageBlock";
 import TableStudentsList from "../TableStudentsList/TableStudentsList";
+import TableStudyPlan from "../TableStudyPlan/TableStudyPlan";
 const StudyPlan = () => {
   const [year, setYear] = React.useState("");
   const [block, setBlock] = React.useState("");
@@ -66,18 +67,99 @@ const StudyPlan = () => {
 
         <div className={StudyPlanCSS.tablesWrapper}>
 
-
+          <div className={StudyPlanCSS.containerWrapperLeft}>
             <div className={`${StudyPlanCSS.tableWrapperLeft}`}>
                 <StudyPlanForm/>
             </div> 
+            <div className={StudyPlanCSS.bottomButtonsLeft}>
+              <Stack spacing={2} direction="row">
+                <Button
+                  // onClick={handleOpen}
+                  style={{ textTransform: "none" }}
+                  sx={{
+                    backgroundColor: "#007bff",
+                    color: "white",
+                    borderRadius: "0.5rem",
+                    fontFamily: "Poppins",
+                    fontSize: "0.9rem",
+                    padding: "0rem",
+                    padding: "0.9rem",
+                    "&:hover": {
+                      backgroundColor: "#0070e7", // Change the hover background color here
+                    },
+                  }}
+                  variant="contained"
+                >
+                  Generate
+                </Button>
+              </Stack>
+            </div>
+            
+          </div>
 
-
+            <div className={StudyPlanCSS.containerWrapperRight}>
             <div className={`${StudyPlanCSS.tableWrapperRight}`}>
-                <TableStudentsList/>
+                <TableStudyPlan/>
             </div> 
+            <div className={StudyPlanCSS.middle}>
+              <Stack spacing={2} direction="row">
+                    <Button
+                      style={{ textTransform: "none" }}
+                      // onClick={() => exportAsPDF(dataChild)}
+                      sx={{ 
+
+                        marginRight: "1rem",
+                        backgroundColor: "#424242",
+
+                        color: "white",
+                        borderRadius: "0.5rem",
+                        fontFamily: "Poppins",
+                        fontSize: isSmallScreen ? "0.6rem" : "0.9rem",
+                        padding: "0rem",
+                        padding: "0.9rem 1.25rem",
+                        "&:hover": {
+                          backgroundColor: "#313131",
+                           // Change the hover background color here
+                        },
+                      }}
+                      variant="contained"
+                    >
+                      Print as PDF
+                    </Button>
+                  </Stack>
+                <Stack spacing={2} direction="row">
+                    <Button
+                      style={{ textTransform: "none" }}
+                      // onClick={() => exportAsExcel(dataChild)}
+                      sx={{ 
+
+                        marginRight: "1rem",
+                        backgroundColor: "#424242",
+
+                        color: "white",
+                        borderRadius: "0.5rem",
+                        fontFamily: "Poppins",
+                        fontSize: isSmallScreen ? "0.6rem" : "0.9rem",
+                        padding: "0rem",
+                        padding: "0.9rem",
+                        "&:hover": {
+                          backgroundColor: "#313131",
+                           // Change the hover background color here
+                        },
+                      }}
+                      variant="contained"
+                    >
+                      Save as XLSX
+                    </Button>
+                  </Stack>
+              </div>
+              
+            </div>
 
 
         </div>
+
+
 
       </div>
 
---
+++ src/components/TableStudyPlan/TableStudyPlan.jsx
@@ -0,0 +1,193 @@
+import React, { useEffect, useState } from 'react';
+import { Table, TableHead, TableRow, TableCell, TableBody, TablePagination } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import * as XLSX from 'xlsx';
+import TableStudyPlanCSS from './TableStudyPlan.module.css'
+
+const StickyPagination = styled('div')({
+  position: 'sticky',
+  bottom: 0,
+  width: '100%',  // make sure it stretches to full width
+  backgroundColor: '#f7f4f4',
+  zIndex: 1,
+  padding: "2px 0px"
+});
+
+
+
+const StyledTableCellID = styled(TableCell)({
+  fontWeight: 'bold',
+  backgroundColor: '#f7f4f4',
+});
+
+const StyleTable = styled(Table)({
+
+  });
+  
+  const StyledTableCellLeft = styled(TableCell)({
+    fontWeight: 'bold',
+    backgroundColor: '#f7f4f4',
+
+  });
+  
+  const StyledTableCellRight = styled(TableCell)({
+    fontWeight: 'bold',
+    backgroundColor: '#f7f4f4',
+
+    borderTopRightRadius: '1rem'
+  });
+  const StyledTableCell = styled(TableCell)({
+    fontWeight: 'bold',
+    backgroundColor: '#f7f4f4',
+  });
+  
+  const StyledTableRow = styled(TableRow)({
+    backgroundColor: '#ffffff',
+  });
+  
+  const StyledTableHead = styled(TableHead)({
+    position: 'sticky',
+    top: 0,
+    zIndex: 1,
+  });
+
+const TableStudyPlan = ({standing, setDataChild, yearButton, blockButton}) => {
+  const [data, setData] = useState([])
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchDataButtons = () => {
+      console.log(standing, 'me!!')
+      fetch(`http://localhost:3000/grabStudentsButtons?standingButton=${standing}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+          setDataChild(data);
+          console.log(data);
+        })
+        .catch((error) => console.log(error));
+    };
+
+    fetchDataButtons();
+  }, [standing]);
+
+  const fetchDataButtonsYear = () => {
+    fetch(
+      `http://localhost:3000/grabStudentsButtons?yearButton=${yearButton}&blockButton=${blockButton}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setDataChild(data);
+
+      })
+      .catch((error) => console.log(error));
+  };
+
+
+
+  const fetchDataButtonsBlock = () => {
+    fetch(
+      `http://localhost:3000/grabStudentsButtons?yearButton=${yearButton}&blockButton=${blockButton}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setDataChild(data);
+
+        console.log("hoho block", data)
+
+      })
+      .catch((error) => console.log(error));
+  };
+
+ 
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+
+  useEffect(() => {
+
+      fetchDataButtonsYear();
+      setPage(0);
+      console.log("i ran year  data");
+   
+  }, [yearButton]);
+
+  useEffect(() => {
+ 
+      fetchDataButtonsBlock();
+      setPage(0);
+      console.log("i ran block data");
+    
+  }, [blockButton]);
+
+  useEffect(() => {
+    setPage(0); // Set the page state to 0
+  }, [data]); // Watch for changes in the data state
+
+  
+
+  return (
+    <div className={TableStudyPlanCSS.studyPlanTableWrapper}>
+
+    <StyleTable>
+    <Table>
+      <StyledTableHead>
+        <StyledTableRow>
+        <StyledTableCellID>ID</StyledTableCellID>
+          <StyledTableCellLeft>Student ID</StyledTableCellLeft>
+          <StyledTableCell>Last Name</StyledTableCell>
+          <StyledTableCell>First Name</StyledTableCell>
+          <StyledTableCell>Middle Name</StyledTableCell>
+          <StyledTableCell>Standing</StyledTableCell>
+          <StyledTableCell>Year</StyledTableCell>
+          <StyledTableCellRight>Block</StyledTableCellRight>
+        </StyledTableRow>
+      </StyledTableHead>
+      <TableBody>
+      {data
+          .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+          .map((row, index) => (
+            <StyledTableRow key={row.id}>
+              <TableCell>{page * rowsPerPage + index + 1}</TableCell>
+              <TableCell>{row.student_id}</TableCell>
+              <TableCell>{row.last_name}</TableCell>
+              <TableCell>{row.first_name}</TableCell>
+              <TableCell>{row.middle_name}</TableCell>
+              <TableCell>{row.standing}</TableCell>
+              <TableCell>{row.year}</TableCell>
+              <TableCell>{row.block}</TableCell>
+            </StyledTableRow>
+        ))}
+      </TableBody>
+    </Table>
+    </StyleTable>
+
+<StickyPagination>
+    <TablePagination
+        component="div"
+        count={data.length}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        rowsPerPageOptions={[10, 25, 50, 100]}
+      
+      />
+      </StickyPagination>
+    </div>
+  );
+};
+
+export default TableStudyPlan;
